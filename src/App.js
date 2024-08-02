fa03b7a7a5a3fddd4cdf209c3410b4441497cac6
ktunchar <maikittitee1615@gmail.com>
2024-08-02 18:32:29 +0700

    use effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
 const App = () => {
 	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		alert("you have changed counter to " + counter);
+	}, [counter]);
 	return (
 	  <div className="App">
 		<button onClick={
